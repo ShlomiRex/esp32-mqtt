f9a0a63d5dfcc57aea3cb56053e4abd2650241cc
Shlomi <vgtvgy1@gmail.com>
2019-05-24 19:48:35 +0300

    First commit

---
+++ mqtt.ino
@@ -0,0 +1,50 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* ssid = "Shark";
+const char* password =  "0542557736";
+const char* mqttServer = "10.0.0.1";
+const int mqttPort = 1883;
+const char* mqttUser = "user1";
+const char* mqttPassword = "pass1";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.publish("esp/test", "Hello from ESP32");
+ 
+}
+ 
+void loop() {
+  client.loop();
+}
